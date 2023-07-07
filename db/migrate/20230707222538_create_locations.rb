1c2ff922674244c85e41ac1fe196b7e7535c13cf
anyepeter <petzyrockchendi@gmail.com>
2023-07-07 23:27:28 +0100

    create location table

---
+++ db/migrate/20230707222538_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :locations do |t|
+      t.string :city
+      t.text :quater
+      t.decimal :longitude
+      t.decimal :latitude
+
+      t.timestamps
+    end
+  end
+end
